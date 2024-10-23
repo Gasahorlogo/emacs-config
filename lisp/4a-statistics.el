72f3b936493598e04ba929d0e40ce34ce74ee14e
Jianghao Gasahorlogo You <gasahorlogo@g-h2.eu.org>
2024-10-23 18:45:38 +0800

    Fix: provide statistics

--- lisp/4a-statistics.el
+++ lisp/4a-statistics.el
@@ -1,6 +1,6 @@
 ;; statistics support
 ;; Birth:  2024-10-20 12:39:34.382668964 +0800
-;; Modify: 2024-10-20 12 +0800
+;; Modify: 2024-10-23 18 +0800
 
 ;; References:
 ;; 1) Using Emacs for R
@@ -16,7 +16,7 @@
 ;(setq company-selection-wrap-around t
 ;      company-tooltip-align-annotations t
 ;      company-idle-delay 0.45
-;      company-minimum-prefix-length 3
+;      company-minimum-prefix-length 2
 ;      company-tooltip-limit 10)
 
 (provide '4a-statistics)
