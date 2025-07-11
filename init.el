1bce1793e18e614f4f49bba18be1787985f0a982
Jianghao Gasahorlogo You <gasahorlogo@g-h2.eu.org>
2025-07-11 16:30:23 +0800

    Fix some configuration of Lisp

--- init.el
+++ init.el
@@ -2,7 +2,7 @@
 
 ;; This is a Lisp program from an initialization file for Emacs by Gasahorlogo.
 ;; Birth:  2024-09-30 18:50:42.142222749 +0800
-;; Modify: 2025-01-05 20 +0800
+;; Modify: 2025-07-11 16 +0800
 
 ;; References:
 ;; 1) GNU Emacs Manual 50.4 The Emacs Initialization File
@@ -14,6 +14,8 @@
  (lambda (dir)
    (add-to-list 'load-path (expand-file-name dir user-emacs-directory)))
  '("lisp"))
+
+(add-hook 'text-mode-hook #'auto-fill-mode)
 
 (tool-bar-mode 0)
 (menu-bar-mode 0)
--- lisp/4a-lisp.el
+++ lisp/4a-lisp.el
@@ -1,36 +1,33 @@
 ;; lisp language preferences
 ;; Birth:  2024-10-13 20:14:16.718719292 +0800
-;; Modify: 2025-01-22 18 +0800
+;; Modify: 2025-07-11 16 +0800
 
 ;; ParEdit
-(use-package paredit
-  :config ;References: EmacsWiki
-  (autoload 'enable-paredit-mode "paredit" "Turn on pseudo-structural editing of Lisp code." t)
-  (add-hook 'emacs-lisp-mode-hook       #'enable-paredit-mode)
-  (add-hook 'eval-expression-minibuffer-setup-hook #'enable-paredit-mode)
-  (add-hook 'ielm-mode-hook             #'enable-paredit-mode)
-  (add-hook 'lisp-mode-hook             #'enable-paredit-mode)
-  (add-hook 'lisp-interaction-mode-hook #'enable-paredit-mode)
-  (add-hook 'scheme-mode-hook           #'enable-paredit-mode))
-
-;; Common Lisp
-(setq inferior-lisp-program "sbcl") ; set default common lisp inferior
+;(use-package paredit
+;  :config                               ;References: EmacsWiki
+;  (autoload 'enable-paredit-mode "paredit" "Turn on pseudo-structural editing of Lisp code." t)
+;  (add-hook 'emacs-lisp-mode-hook       #'enable-paredit-mode)
+;  (add-hook 'eval-expression-minibuffer-setup-hook #'enable-paredit-mode)
+;  (add-hook 'ielm-mode-hook             #'enable-paredit-mode)
+;  (add-hook 'lisp-mode-hook             #'enable-paredit-mode)
+;  (add-hook 'lisp-interaction-mode-hook #'enable-paredit-mode)
+;  (add-hook 'scheme-mode-hook           #'enable-paredit-mode))
 
 ;; SLIME
 (use-package slime
   :ensure t)
 
-;; Stop SLIME's REPL from grabbing DEL,
-;; which is annoying when backspacing over a '('
-(defun override-slime-repl-bindings-with-paredit ()
-(define-key slime-repl-mode-map
-            (read-kbd-macro paredit-backward-delete-key) nil))
-(add-hook 'slime-repl-mode-hook 'override-slime-repl-bindings-with-paredit)
+(add-hook 'lisp-mode-hook (lambda () (slime-mode t)))
+(slime-setup '(slime-fancy))
+
+;; Common Lisp
+(setq inferior-lisp-program "/usr/bin/ecl") ; set default common lisp inferior
 
 ;; Scheme
-;; Chez-Scheme
-(setq geiser-chez-binary "/usr/bin/chez-scheme")
-(setq geiser-active-implementations '(chez))
+;; Geiser
+(setq geiser-active-implementations '(chicken))
+
+;(setq scheme-program-name "csi")
 
 ;; provide
 (provide '4a-lisp)
